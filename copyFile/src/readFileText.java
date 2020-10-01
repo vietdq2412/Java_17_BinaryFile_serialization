6ae4a386be51208a3352ac914dc8bb1acc0a8d28
viet <vietdq2412@gmail.com>
2020-10-01 11:09:38 +0700

    add copyFile

---
+++ copyFile/src/readFileText.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
+
+public class readFileText {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter source file:");
+        String sourcePath = sc.nextLine();
+        System.out.printf("Enter destination file:");
+        String destPath = sc.nextLine();
+
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+
+        try {
+            copyFileUsingJava7Files(sourceFile, destFile);
+            //copyFileUsingStream(sourceFile, destFile);
+            System.out.printf("Copy completed");
+        } catch (IOException ioe) {
+            System.out.printf("Can't copy that file");
+            System.out.printf(ioe.getMessage());
+        }
+    }
+/////////////////////////////h
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
+        Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            is.close();
+            os.close();
+        }
+    }
+}
